7dd1821987300eacbe75ac5e61ff8d4592898623
Alexandre <alexandre.delfosse@efrei.net>
2020-11-06 16:24:04 +0100

    Start dev

--- Fizzbuzz.c
+++ Fizzbuzz.c
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 #define FIZZ "Fizz"
 #define BUZZ "Buzz"
-int main(void) {
-	for (int i = 1; i <= 100; i++) {
-		if (i % 15 == 0) {
+int main(void) 
+{
+	for (int i = 1; i <= 100; i++) 
+	{
+		if (i % 15 == 0) 
+		{
 			printf("%s%s\n", FIZZ, BUZZ);
 		}
-		else if (i % 3 == 0) {
+		else if (i % 3 == 0) 
+		{
 			printf("%s\n", FIZZ);
 		}
-		else if (i % 5 == 0) {
+		else if (i % 5 == 0) 
+		{
 			printf("%s\n", BUZZ);
 		}
-		else {
+		else 
+		{
 			printf("%d\n", i);
 		}
 	}
 	return EXIT_SUCCESS;
-}
+}
